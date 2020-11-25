51b3b6710b4070224e691dabd45e3c892d48e693
unknown <unknown@example.com>
2020-11-25 16:42:41 +0800

    Initial Commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  version: '9.3.1',
+  note: '分类滚动bug', // 这个为版本描述，无需修改
+  subDomain: '378d47b47c188b6974d8008292958e91', // 此处改成你自己的专属域名。什么是专属域名？请看教程 https://www.it120.cc/help/qr6l4m.html
+}
